f82492f0a872e3dc970def8ffa23d5f839b7fc34
JunxiBao <baojunxilogin@icloud.com>
2025-01-03 13:51:05 +0800

    add gitGUI

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,13 @@
 return {
   {
-    "tpope/vim-fugitive",
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }, -- 依赖库
+    cmd = "LazyGit", -- 仅在运行命令时加载
+    keys = {
+      { "<leader>lg", ":LazyGit<CR>", desc = "Open LazyGit" }, -- 快捷键绑定
+    },
+    config = function()
+      -- 如果需要额外配置，可以在这里添加
+    end,
   },
 }
