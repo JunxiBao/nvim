ce17d5432199b441b9a43f7697e79675c7f007f4
JunxiBao <baojunxilogin@icloud.com>
2025-01-08 21:20:43 +0800

    Add markdown preview

---
+++ lua/plugins/Markdown.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      highlight_code_blocks = true, -- 高亮 Markdown 代码块
+      default_preview_engine = "markdown-preview", -- 使用 markdown-preview 引擎
+      enable_math_rendering = true, -- 启用数学公式渲染},
+    },
+  },
+}
