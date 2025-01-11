cd466a7a6626db1a5b8fe31906aac210290de2d9
JunxiBao <baojunxilogin@icloud.com>
2025-01-11 22:11:46 +0800

    add AI, change terminal and remove animate

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
     require("toggleterm").setup({
       size = 20, -- 设置浮动终端的大小
       open_mapping = [[<C-\>]], -- 绑定打开/关闭终端的快捷键
-      direction = "float", -- 设置浮动模式
+      direction = "horizontal", -- 设置浮动模式
       shade_terminals = true, -- 设置终端阴影效果
       start_in_insert = true, -- 打开终端自动进入插入模式
     })
