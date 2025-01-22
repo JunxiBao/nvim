eb940fef28a9420c0397016f3a16be69351b297f
JunxiBao <baojunxilogin@icloud.com>
2025-01-22 12:23:15 +0800

    add soothcursor and change the size of terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      size = 20, -- 设置浮动终端的大小
+      size = 10, -- 设置浮动终端的大小
       open_mapping = [[<C-\>]], -- 绑定打开/关闭终端的快捷键
       direction = "horizontal", -- 设置浮动模式
       shade_terminals = true, -- 设置终端阴影效果
